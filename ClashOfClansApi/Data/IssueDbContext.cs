7600abc633ffdc75f9a4a26dcd962a177e22687f
BaleusBlueberry <ssaasshh29@gmail.com>
2024-04-12 09:10:31 +0300

    made a IssueDbContext

---
+++ ClashOfClansApi/Data/IssueDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ClashOfClansApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Client;
+
+namespace ClashOfClansApi.Data
+{
+    public class IssueDbContext : DbContext {
+        
+        public IssueDbContext(DbContextOptions<IssueDbContext> options) : base(options)
+        {
+
+        }
+    public DbSet<Issue> Issues
+    {
+        get;
+        set;
+    }
+
+    }
+
+    
+
+}
